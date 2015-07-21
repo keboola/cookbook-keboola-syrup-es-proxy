2646fdfa7e6d1586eb47556e9ddda22f98820246
Martin Halamíček <martin@halamicek.cz>
2015-07-21 10:20:23 +0200

    proxy settings

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['apache']['default_modules'] = %w[
+  dir env mime negotiation setenvif
+  status alias auth_basic authn_core authn_file authz_core authz_groupfile
+  authz_host authz_user autoindex dir env mime negotiation setenvif logio log_config unixd socache_shmcb filter
+  proxy proxy_http
+]
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,10 @@
 include_recipe "keboola-common"
 include_recipe "keboola-php56"
 include_recipe "keboola-apache2"
+
+web_app "#{node['fqdn']}" do
+  template "proxy.conf.erb"
+  server_name node['fqdn']
+  server_aliases [node['hostname'], 'syrup-es-proxy.keboola.com']
+  enable true
+end
